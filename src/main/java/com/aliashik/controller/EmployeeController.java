42de58235d0edf5b35f77fe4e519563765e96b58
r.ali.ashik <ramjan.ali.ashik@gmail.com>
2018-09-01 06:44:19 +0600

    adding swagger2

--- src/main/java/com/aliashik/controller/EmployeeController.java
+++ src/main/java/com/aliashik/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import com.aliashik.dto.EmployeeDTO;
 import com.aliashik.exception.EmployeeNotFoundException;
 import com.aliashik.model.Employee;
 import com.aliashik.repository.EmployeeRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,33 +19,39 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/v1")
+@Api(tags = "Employee", description = "Employee Resource", value = "employee")
 class EmployeeController {
 
     @Autowired
     private EmployeeRepository repository;
 
-    @GetMapping("/employees")
+    @ApiOperation(value = "Get employees", notes = "This api fetches all of the employees, no param is required")
+    @GetMapping(value = "/employees", produces = "application/json")
     public ResponseEntity<List<Employee>> getEmployees() {
         return new ResponseEntity(repository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping("/employees")
-    public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+    @ApiOperation(value = "Create an Employee", notes = "This api creates an Employee, corresponding json is required")
+    @PostMapping(value = "/employees", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Employee> createEmployee(
+            @ApiParam(value = "employee json", required = true) @RequestBody EmployeeDTO employeeDTO) {
+
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
         return new ResponseEntity(repository.save(employee), HttpStatus.CREATED);
     }
 
-    @GetMapping("/employees/{id}")
-    public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+    @ApiOperation(value = "Get Employee wit id", notes = "This api gets an Employee with employee id")
+    @GetMapping(value = "/employees/{id}", produces = "application/json")
+    public ResponseEntity<Employee> getEmployee(
+            @ApiParam(value = "id", required = true) @PathVariable Long id) {
 
         return new ResponseEntity(repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException("Employee Not Found")), HttpStatus.OK);
     }
 
-    @PutMapping("/employees/{id}")
+    @PutMapping(value = "/employees/{id}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Employee> updateEmployee(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long id) {
-
 
         return repository.findById(id)
                 .map(employee -> {
@@ -58,7 +67,7 @@ class EmployeeController {
                 });
     }
 
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping(value = "/employees/{id}", produces = "application/json")
     public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
