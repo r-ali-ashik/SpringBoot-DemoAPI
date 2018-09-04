40258742744585237e60fd24e69d8f35d632ad7a
r.ali.ashik <ramjan.ali.ashik@gmail.com>
2018-09-04 20:02:52 +0600

    configuring custom swagger-ui

--- src/main/java/com/aliashik/config/SwaggerConfig.java
+++ src/main/java/com/aliashik/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ package com.aliashik.config;
 import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -17,17 +19,17 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.aliashik.controller"))
                 .paths(paths())
                 .build()
                 .apiInfo(metaData());
-
     }
 
     private Predicate<String> paths() {
@@ -38,10 +40,19 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("Demo Rest API")
                 .description("Spring Boot demo REST API")
-                .version("1.0.0")
+   /*             .version("1.0.0")
                 .license("MIT License")
                 .licenseUrl("https://opensource.org/licenses/MIT")
-                .contact(new Contact("Ali Ashik", "www.facebook.com/r.ali.ashik", "ramjan.ali.ashik@gmail.com"))
+                .contact(new Contact("Ali Ashik", "www.facebook.com/r.ali.ashik", "ramjan.ali.ashik@gmail.com"))*/
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/static/swagger/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/static/swagger/webjars/");
     }
 }
